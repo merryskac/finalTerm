ce1109facbd16c09cd85f300f4f2ed2f3a0fdd12
unknown <merryskachr@gmail.com>
2023-08-14 07:53:01 +0800

    db

--- be/backend/api/server.js
+++ be/backend/api/server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(cors({credentials: true, origin:'http://localhost:5173'}))
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin","http://192.168.1.8:5173");
+  res.setHeader("Access-Control-Allow-Origin","http://localhost:5173");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
