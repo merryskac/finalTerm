0cd2b1f1607494c25e63edee68a6dd11e2490374
unknown <merryskachr@gmail.com>
2023-08-14 00:36:15 +0800

    db

--- be/backend/server.js
+++ be/backend/server.js
@@ -8,14 +8,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors'
 
 mongoose.connect(process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-    function (err, res) {
-        try {
-            console.log('Connected to Database');
-        } catch (err) {
-            throw err;
-        }
-    })
+  { useNewUrlParser: true, useUnifiedTopology: true }).then((err, res)=>{
+    if(err){console.log(err)}
+    console.log('connect to db')
+  })
 // const database = mongoose.connection;
 
 // database.once('connected',()=>{
