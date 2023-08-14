e54d0058ae317ad7a28b9ec5908899eb02bf09f9
unknown <merryskachr@gmail.com>
2023-08-15 07:26:37 +0800

    netlify

--- fe/frontend/src/logout/logoutAPI.jsx
+++
@@ -1,9 +0,0 @@
-export const logout = () =>{
-  const out = fetch('https://final-proj-blond.vercel.app/play/logout',{
-    credentials: 'include'
-  }).then(
-    data=>data.json()
-  ).then(data=>data)
-
-  return out
-}
